50b60e35b0f926eee94cc577f12f8abac160a746
Moisés Souza <86939796+moisessouzapy@users.noreply.github.com>
2022-03-03 01:00:09 -0300

    Update questao3.java

--- src/senai1/questao3.java
+++ src/senai1/questao3.java
@@ -1,6 +1,46 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 3) Programa Comida.java
+Escreva um programa com 3 funções:
+Função main:
+● Possui a variável precoTotal, para armazenar o valor da compra.
+● Possui um menu inicial, que solicita que a pessoa informe o que
+deseja comer: D) Doce, S) Salgado ou Z) Sair.
+○ Para capturar o char, use next().charAt(0).
+● Se a pessoa digitar um caractere inválido, exiba uma mensagem de
+alerta e o menu inicial novamente.
+● Caso a pessoa digite D, chama a função menuDoce(), passando
+como parâmetro a variável do Scanner. O retorno dessa função é
+SOMADO à variável precoTotal.
+● Caso a pessoa digite S, chama a função menuSalgado(), passando
+como parâmetro a variável do Scanner. O retorno dessa função
+também é SOMADO à variável precoTotal.
+● Quando a pessoa finaliza a compra do doce/salgado, o programa
+volta ao menu inicial.
+
+● Caso a pessoa digite Z, o programa exibe o valor total da compra e
+é encerrado.
+
+Função menuDoce():
+● Possui um parâmetro do tipo Scanner e retorna um float.
+
+● Exibe um menu com três doces: 1) Brigadeiro; 2) Cocada ou 3) Pé-
+de-moleque.
+
+● Caso a pessoa digite um número diferente das opções válidas, o
+programa reexibe o menu de doces.
+● Quando uma opção válida é escolhida, o programa retorna o preço
+do doce. Qualquer doce custa R$ 2,00.
+Função menuSalgado():
+● Possui um parâmetro do tipo Scanner e retorna um float.
+● Exibe um menu com quatro salgados: 1) Pastel; 2) Empada; 3)
+Coxinha ou 4) Esfirra.
+● Caso a pessoa digite um número diferente das opções válidas, o
+programa reexibe o menu de salgados.
+● Quando uma opção válida é escolhida, o programa retorna o preço
+do salgado. Pastel ou empada custa R$ 5,00. Coxinha ou esfirra
+custa R$ 3,00.
  */
 package senai1;
 
